1a24d96fc14f0c7f042ab3379072bc2acb2d75ee
FurkanHome <FurkanHome@FURKANPC>
2020-08-04 15:12:30 +0300

    queue and stack added

---
+++ Queue/Queue.h
@@ -0,0 +1,361 @@
+﻿#pragma once
+//Queue.hpp
+#ifndef KUYRUK_HPP
+#define KUYRUK_HPP
+
+#include <cmath>
+template <typename Object>
+class Queue {
+private:
+	int front;
+	int back;
+	int capacity;
+	int length;
+	Object* items;
+
+	void reserve(int yeniKapasite) {
+		Object* temp = new Object[yeniKapasite];
+		for (int i = front, j = 0; j < length; j++) {
+			temp[j] = items[i];
+			i = (i + 1) % capacity;
+		}
+		capacity = yeniKapasite;
+		delete[] items;
+		items = temp;
+		front = 0;
+		back = length - 1;
+	}
+public:
+	Queue() {
+		capacity = 1;
+		front = 0;
+		back = -1;
+		length = 0;
+		items = new Object[capacity];
+	}
+	void clear() {
+		front = 0;
+		back = -1;
+		length = 0;
+	}
+	int count() const {
+		return length;
+	}
+	bool isEmpty() const {
+		if (length == 0) return true;
+		return false;
+	}
+	/// Kuyrukta en öndeki elemanı getirir
+	const Object& peek() const {
+		if (isEmpty())
+			return NULL;
+		return items[front];
+	}
+	void enqueue(const Object& item) {
+		if (length == capacity) reserve(2 * capacity);
+		back = (back + 1) % capacity;
+		items[back] = item;
+		length++;
+	}
+	void dequeue() {
+		if (isEmpty())
+			return;
+		front = (front + 1) % capacity;
+		length--;
+	}
+	~Queue() {
+		delete[] items;
+	}
+};
+#endif
+//Test.cpp
+#include "Queue.hpp"
+#include <iostream>
+using namespace std;
+struct Ogrenci {
+	int no;
+	string adSoyad;
+	Ogrenci(int numara, string isim) {
+		no = numara;
+		adSoyad = isim;
+	}
+	friend ostream& operator<<(ostream& ekran, Ogrenci& sag) {
+		ekran << sag.no << " " << sag.adSoyad << endl;
+		return ekran;
+	}
+};
+
+int main() {
+	Queue<int>* kuyruk = new Queue<int>();
+	kuyruk->enqueue(15);
+	kuyruk->enqueue(16);
+	kuyruk->enqueue(17);
+	kuyruk->enqueue(18);
+	kuyruk->enqueue(19);
+	kuyruk->enqueue(20);
+	kuyruk->enqueue(21);
+
+	kuyruk->dequeue();
+	kuyruk->dequeue();
+	kuyruk->dequeue();
+
+	kuyruk->enqueue(1);
+	kuyruk->enqueue(2);
+	kuyruk->enqueue(3);
+	kuyruk->enqueue(4);
+	kuyruk->enqueue(5);
+	kuyruk->enqueue(6);
+	kuyruk->enqueue(7);
+	cout << kuyruk->peek() << endl << endl;
+	delete kuyruk;
+
+	Queue<Ogrenci*>* kuyruk2 = new Queue<Ogrenci*>();
+	Ogrenci* ali = new Ogrenci(224, "Ali Yilmaz");
+	Ogrenci* mehmet = new Ogrenci(100, "Mehmet Korkmaz");
+	Ogrenci* veli = new Ogrenci(500, "veli sari");
+	kuyruk2->enqueue(ali);
+	kuyruk2->enqueue(mehmet);
+	cout << *(kuyruk2->peek()) << endl;
+	kuyruk2->dequeue();
+	cout << *(kuyruk2->peek()) << endl;
+	kuyruk2->enqueue(veli);
+	kuyruk2->enqueue(ali);
+	cout << *(kuyruk2->peek()) << endl;
+	delete ali;
+	delete mehmet;
+	delete veli;
+	delete kuyruk2;
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / bin / test . / src / Test.cpp
+//
+//calistir :
+//. / bin / test
+
+//Kuyruk Liste, Queue'yi kopyala
+//Queue.hpp
+#ifndef KUYRUK_HPP
+#define KUYRUK_HPP
+#include <iostream>
+
+template <typename Object>
+class Node {
+public:
+	Object item;
+	Node<Object>* next;
+	Node(Object itm, Node<Object>* nxt = NULL) {
+		item = itm;
+		next = nxt;
+	}
+};
+
+template <typename Object>
+class Queue {
+private:
+	Node<Object>* front;
+	Node<Object>* back;
+	int length;
+
+public:
+	Queue() {
+		front = back = NULL;
+		length = 0;
+	}
+	void clear() {
+		while (!isEmpty()) dequeue();
+	}
+	int count() const {
+		return length;
+	}
+	bool isEmpty() const {
+		if (length == 0) return true;
+		return false;
+	}
+	/// Kuyrukta en öndeki elemanı getirir
+	const Object& peek() {
+		if (isEmpty())
+			return NULL;
+		return front->item;
+	}
+	void enqueue(const Object& item) {
+		Node<Object>* last = new Node<Object>(item);
+		if (isEmpty()) front = back = last;
+		else {
+			back->next = last;
+			back = last;
+		}
+		length++;
+	}
+	void dequeue() {
+		if (isEmpty())
+			return;
+		Node<Object>* eskiOn = front;
+		front = front->next;
+		delete eskiOn;
+		length--;
+	}
+	~Queue() {
+		clear();
+	}
+};
+#endif
+//Test.cpp
+#include "Queue.hpp"
+using namespace std;
+struct Ogrenci {
+	int no;
+	string adSoyad;
+	Ogrenci(int numara, string isim) {
+		no = numara;
+		adSoyad = isim;
+	}
+	friend ostream& operator<<(ostream& ekran, Ogrenci& sag) {
+		ekran << sag.no << " " << sag.adSoyad << endl;
+		return ekran;
+	}
+};
+
+int main() {
+	Queue<Ogrenci*>* kuyruk = new Queue<Ogrenci*>();
+	Ogrenci* ali = new Ogrenci(224, "Ali Yilmaz");
+	Ogrenci* mehmet = new Ogrenci(100, "Mehmet Korkmaz");
+	Ogrenci* veli = new Ogrenci(500, "veli sari");
+	kuyruk->enqueue(ali);
+	kuyruk->enqueue(mehmet);
+	cout << *(kuyruk->peek()) << endl;
+	kuyruk->dequeue();
+	cout << *(kuyruk->peek()) << endl;
+	kuyruk->enqueue(veli);
+	kuyruk->enqueue(ali);
+	cout << *(kuyruk->peek()) << endl;
+	delete ali;
+	delete mehmet;
+	delete veli;
+	delete kuyruk;
+	return 0;
+}
+//Öncelikli kuyruk, kuyruklisteyi kopyala
+//Queue.hpp
+#ifndef KUYRUK_HPP
+#define KUYRUK_HPP
+#include <iostream>
+
+template <typename Object>
+class Node {
+public:
+	Object item;
+	Node<Object>* next;
+	Node(Object itm, Node<Object>* nxt = NULL) {
+		item = itm;
+		next = nxt;
+	}
+};
+
+template <typename Object>
+class Queue {
+private:
+	Node<Object>* front;
+	Node<Object>* back;
+	int length;
+
+	Node<Object>* previousMaxNode() {
+		Node<Object>* prev = NULL;
+		Node<Object>* tmp = front;
+		Object max = front->item;
+		while (tmp->next != NULL) {
+			if (tmp->next->item > max) {
+				max = tmp->next->item;
+				prev = tmp;
+			}
+			tmp = tmp->next;
+		}
+		return prev;
+	}
+public:
+	Queue() {
+		front = back = NULL;
+		length = 0;
+	}
+	void clear() {
+		while (!isEmpty()) dequeue();
+	}
+	int count() const {
+		return length;
+	}
+	bool isEmpty() const {
+		if (length == 0) return true;
+		return false;
+	}
+	/// Kuyrukta en öndeki elemanı getirir
+	const Object& peek() {
+		if (isEmpty())
+			return NULL;
+		return front->item;
+	}
+	const Object& peekMax() {
+		if (isEmpty())
+			return NULL;
+		Node<Object>* prev = previousMaxNode();
+		if (prev == NULL)
+			return peek();
+		return prev->next->item;
+	}
+	void enqueue(const Object& item) {
+		Node<Object>* last = new Node<Object>(item);
+		if (isEmpty()) front = back = last;
+		else {
+			back->next = last;
+			back = last;
+		}
+		length++;
+	}
+	void dequeue() {
+		if (isEmpty())
+			return;
+		Node<Object>* eskiOn = front;
+		front = front->next;
+		delete eskiOn;
+		length--;
+	}
+	void dequeueMax() {
+		if (isEmpty())
+			return;
+		Node<Object>* prev = previousMaxNode();
+		Node<Object>* tmp;
+		if (prev == NULL) {
+			dequeue();
+		}
+		else {
+			if (prev->next == back) back = prev; // en son eleman silinecekse
+			tmp = prev->next;
+			prev->next = prev->next->next;
+			delete tmp;
+			length--;
+		}
+	}
+	~Queue() {
+		clear();
+	}
+};
+#endif
+//Test.cpp
+#include "Queue.hpp"
+using namespace std;
+int main() {
+	Queue<int>* kuyruk = new Queue<int>();
+
+	kuyruk->enqueue(724);
+	kuyruk->enqueue(1225);
+	kuyruk->enqueue(531);
+	kuyruk->enqueue(850);
+	kuyruk->dequeueMax();
+	cout << kuyruk->peek() << endl;
+	cout << kuyruk->peekMax() << endl;
+
+	delete kuyruk;
+	return 0;
+}
---
+++ Stack/stack.h
@@ -0,0 +1,392 @@
+﻿#pragma once
+//Stack.hpp
+#ifndef STACK_HPP
+#define STACK_HPP
+#include <iostream>
+using namespace std;
+
+template <typename Object>
+class Stack {
+private:
+	Object* items;
+	int head;
+	int length;
+	int capacity;
+	bool IsFull() {
+		return length == capacity;
+	}
+	void Reserve(int capacity)
+	{
+		Object* tmp = new Object[capacity];
+		for (int j = 0; j < length; j++)
+			tmp[j] = items[j];
+		if (items != NULL)
+			delete[] items;
+		items = tmp;
+		this->capacity = capacity;
+	}
+public:
+	Stack() {
+		items = NULL;
+		head = -1;
+		length = 0;
+		capacity = 0;
+	}
+	bool IsEmpty() const {
+		return head == -1;
+	}
+	void Push(const Object& obj) {
+		if (IsFull()) {
+			Reserve(max(1, 2 * capacity));
+		}
+		head++;
+		items[head] = obj;
+		length++;
+	}
+	void Pop() {
+		if (IsEmpty())
+			return;
+		head--;
+		length--;
+	}
+	const Object& Top() const {
+		if (IsEmpty())
+			return NULL;
+		return items[head];
+	}
+	void Clear() {
+		head = -1;
+		length = 0;
+	}
+	~Stack() {
+		if (items != NULL)
+			delete[] items;
+		items = NULL;
+	}
+};
+#endif
+//Test.cpp
+#include "Stack.hpp"
+
+int main()
+{
+	Stack<int>* s1 = new Stack<int>();
+	s1->Push(25);
+	s1->Push(32);
+	s1->Push(68);
+	s1->Push(74);
+	s1->Pop();
+	cout << s1->Top();
+	delete s1;
+	cout << endl;
+
+	Stack<string>* s2 = new Stack<string>();
+	s2->Push("Mehmet");
+	s2->Push("Veli");
+	s2->Push("Ahmet");
+	s2->Push("Sakarya");
+	s2->Pop();
+	cout << s2->Top();
+	delete s2;
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / bin / test . / src / Test.cpp
+//calistir :
+//. / bin / test
+//StackList.hpp Stack projesini kopyalarayak oluştur
+#ifndef STACKLIST_HPP
+#define STACKLIST_HPP
+#include <iostream>
+using namespace std;
+
+template <typename Object>
+class Node {
+public:
+	Object data;
+	Node<Object>* next;
+	Node(const Object& data, Node* next = NULL) {
+		this->data = data;
+		this->next = next;
+	}
+};
+
+
+template <typename Object>
+class Stack {
+private:
+	Node<Object>* head;
+public:
+	Stack() {
+		head = NULL;
+	}
+	bool IsEmpty() const {
+		return head == NULL;
+	}
+	void Push(const Object& obj) {
+		head = new Node<Object>(obj, head);
+	}
+	void Pop() {
+		if (IsEmpty())
+			return;
+		Node<Object>* tmp = head;
+		head = head->next;
+		delete tmp;
+	}
+	const Object& Top() const {
+		if (IsEmpty())
+			return NULL;
+		return head->data;
+	}
+	void Clear() {
+		while (!IsEmpty())
+			Pop();
+	}
+	~Stack() {
+		Clear();
+	}
+};
+#endif
+//Taban dönüştürme
+//StackList'i kopyala
+//Sayi.hpp
+#ifndef SAYI_HPP
+#define SAYI_HPP
+
+#include "StackList.hpp"
+
+class Sayi {
+private:
+	int deger;
+	int taban;
+	string donusturulen;
+
+	string TabanDonustur();
+public:
+	Sayi();
+	int Taban10Deger()const;
+	friend ostream& operator<<(ostream&, Sayi&);
+	friend istream& operator>>(istream&, Sayi&);
+};
+
+#endif
+//Sayi.cpp
+#include "Sayi.hpp"
+string Sayi::TabanDonustur() {
+	int sayi = deger;
+	string rakamlar = "0123456789ABCDEF";
+	string yeniSayi = "";
+	Stack<int>* yigit = new Stack<int>();
+
+	while (sayi > 0) {
+		int kalan = sayi % taban;
+		yigit->Push(kalan);
+		sayi = sayi / taban;
+	}
+
+	while (!yigit->IsEmpty()) {
+		yeniSayi = yeniSayi + rakamlar[yigit->Top()];
+		yigit->Pop();
+	}
+	delete yigit;
+	return yeniSayi;
+}
+Sayi::Sayi() {
+	cin >> *this;
+	donusturulen = TabanDonustur();
+}
+int Sayi::Taban10Deger()const {
+	return deger;
+}
+ostream& operator<<(ostream& ekran, Sayi& sag) {
+	ekran << "(" << sag.deger << ")10" << "=" << "(" << sag.donusturulen << ")" << sag.taban;
+}
+istream& operator>>(istream& girdi, Sayi& sag) {
+	cout << "Sayi:";
+	girdi >> sag.deger;
+	cout << "Taban:";
+	girdi >> sag.taban;
+	return girdi;
+}
+//Test.cpp
+#include "Sayi.hpp"
+
+int main() {
+	Sayi* s = new Sayi();
+	cout << *s;
+	delete s;
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / lib / Sayi.o - c . / src / Sayi.cpp
+//g++ - I . / include / -o . / bin / Test . / lib / Sayi.o . / src / Test.cpp
+//
+//calistir :
+//. / bin / Test
+
+//infixToPostfix
+//StackList'i kopyala
+//Postfix.hpp
+#ifndef POSTFIX_HPP
+#define POSTFIX_HPP
+
+#include "StackList.hpp"
+#include <cctype>
+#include <cstdlib>
+
+class Postfix {
+private:
+	string ifade; //postfix ifade
+
+	bool OncelikDusukmu(char, char);
+	void PostfixAktar(Stack<char>*, string&);
+	string infixToPostfix(string);
+public:
+	Postfix(string);
+	double Hesapla();
+	string postfix()const;
+	friend ostream& operator<<(ostream&, Postfix&);
+};
+
+#endif
+//Postfix.cpp
+#include "Postfix.hpp"
+bool Postfix::OncelikDusukmu(char op1, char op2) {//op2'nin önceliği op1'den düşükse true değilse false döner
+	if (op1 == '(') return false;
+	else if (op2 == '(') return false;
+	else if (op2 == ')')return true;
+	else if (op1 == '*' || op1 == '/') return true;
+	else if (op2 == '*' || op2 == '/') return false;
+	else return true;
+}
+void Postfix::PostfixAktar(Stack<char>* yigit, string& postfix) {
+	postfix += yigit->Top();
+	postfix += " ";
+	yigit->Pop();
+}
+string Postfix::infixToPostfix(string infix) {
+	int uzunluk = infix.length();
+	Stack<char>* stack = new Stack<char>();
+	string postfix = "";
+	for (int i = 0; i < uzunluk; i++) {
+		if (isdigit(infix[i])) {
+			while (isdigit(infix[i])) {
+				postfix += infix[i];
+				i++;
+			}
+			postfix += " ";
+			i--;
+		}
+		else if (infix[i] == '(') {
+			stack->Push(infix[i]);
+		}
+		else if (infix[i] == '*' || infix[i] == '+' || infix[i] == '-' || infix[i] == '/') {
+			while (!stack->IsEmpty() && stack->Top() != '(') {
+				if (OncelikDusukmu(stack->Top(), infix[i])) {
+					PostfixAktar(stack, postfix);
+				}
+				else {
+					break;
+				}
+			}
+			stack->Push(infix[i]);
+		}
+		else if (infix[i] == ')') {
+			while (!stack->IsEmpty() && stack->Top() != '(') {
+				PostfixAktar(stack, postfix);
+			}
+			if (!stack->IsEmpty()) stack->Pop();
+		}
+	}
+	while (!stack->IsEmpty()) {
+		PostfixAktar(stack, postfix);
+	}
+	delete stack;
+	return postfix;
+}
+Postfix::Postfix(string infix) {
+	ifade = infixToPostfix(infix);
+}
+double Postfix::Hesapla() {
+	double sonuc;
+	int uzunluk = ifade.length();
+	Stack<double>* stack = new Stack<double>();
+	for (int i = 0; i < uzunluk; i++) {
+		if (isspace(ifade[i])) continue;
+		if (isdigit(ifade[i])) {
+			string sayi = "";
+			while (isdigit(ifade[i])) {
+				sayi += ifade[i];
+				i++;
+			}
+			double s;
+			s = atof(sayi.c_str());
+			stack->Push(s);
+			i--;
+			continue;
+		}
+		else {
+			double sayi2 = stack->Top();
+			stack->Pop();
+			double sayi1 = stack->Top();
+			stack->Pop();
+			switch (ifade[i]) {
+			case '+':
+				stack->Push(sayi1 + sayi2);
+				break;
+			case '-':
+				stack->Push(sayi1 - sayi2);
+				break;
+			case '*':
+				stack->Push(sayi1 * sayi2);
+				break;
+			case '/':
+				stack->Push(sayi1 / sayi2);
+				break;
+			}
+		}
+	}
+	if (!stack->IsEmpty()) {
+		sonuc = stack->Top();
+		stack->Pop();
+	}
+	delete stack;
+	return sonuc;
+}
+string Postfix::postfix()const {
+	return ifade;
+}
+ostream& operator<<(ostream& ekran, Postfix& sag) {
+	ekran << sag.ifade << endl;
+	return ekran;
+}
+//Test.cpp
+#include "Postfix.hpp"
+
+int main()
+{
+	string infix;
+	cout << "Infix ifade:";
+	cin >> infix;
+	Postfix* p = new Postfix(infix);
+	cout << "Postfix: " << *p << endl;
+	cout << "Sonuc=" << p->Hesapla() << endl;
+
+	delete p;
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / lib / Postfix.o - c . / src / Postfix.cpp
+//g++ - I . / include / -o . / bin / Test . / lib / Postfix.o . / src / Test.cpp
+//
+//calistir :
+//. / bin / Test
