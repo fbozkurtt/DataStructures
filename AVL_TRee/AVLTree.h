c11294940406e1a3e1cd87e224e5e8a4e45862dc
FurkanSAU <FurkanSAU@FURKANPC>
2020-08-18 15:26:04 +0300

    avl and heap trees are done

---
+++ AVL_TRee/AVLTree.h
@@ -0,0 +1,237 @@
+﻿#pragma once
+//AVLTree.hpp
+#ifndef AVLTREE_HPP
+#define AVLTREE_HPP
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+template <typename Nesne>
+struct Dugum {
+	Nesne veri;
+	Dugum<Nesne>* sol;
+	Dugum<Nesne>* sag;
+	int yukseklik;
+	Dugum(const Nesne& vr, Dugum<Nesne>* sl = NULL, Dugum<Nesne>* sg = NULL) {
+		veri = vr;
+		sol = sl;
+		sag = sg;
+		yukseklik = 0;//düğümün yüksekliği başlangıçta sıfırdır
+	}
+};
+
+template <typename Nesne>
+class AVLTree {
+private:
+	Dugum<Nesne>* root;
+
+	Dugum<Nesne>* AraveEkle(Dugum<Nesne>* alt_Dugum, const Nesne& yeni) {
+		if (alt_Dugum == NULL) {
+			alt_Dugum = new Dugum<Nesne>(yeni);
+		}
+		else if (yeni < alt_Dugum->veri) {//küçükse sola ekler
+			alt_Dugum->sol = AraveEkle(alt_Dugum->sol, yeni);//sol alt çocuk yerine yeni bir ağaç oluşturur
+
+			if (Yukseklik(alt_Dugum->sol) - Yukseklik(alt_Dugum->sag) == 2) {//yükseklik farkı birden büyükse dengeler
+				// Sol Sol Durumu 
+				if (yeni < alt_Dugum->sol->veri)
+					alt_Dugum = SolCocukIleDegistir(alt_Dugum);//tek dönüş
+				else { //Sol Sağ Durumu
+					alt_Dugum->sol = SagCocukIleDegistir(alt_Dugum->sol); //çift dönüş
+					alt_Dugum = SolCocukIleDegistir(alt_Dugum);
+				}
+			}
+		}
+		else if (yeni > alt_Dugum->veri) {//büyükse sağa ekler
+			alt_Dugum->sag = AraveEkle(alt_Dugum->sag, yeni);
+
+			if (Yukseklik(alt_Dugum->sag) - Yukseklik(alt_Dugum->sol) == 2) {
+				// Sağ Sağ Durumu
+				if (yeni > alt_Dugum->sag->veri)
+					alt_Dugum = SagCocukIleDegistir(alt_Dugum);
+				else {// Sağ Sol Durumu 
+					alt_Dugum->sag = SolCocukIleDegistir(alt_Dugum->sag);
+					alt_Dugum = SagCocukIleDegistir(alt_Dugum);
+				}
+			}
+		}
+		else; // bişey yapma
+
+		//Yüksekliği güncelle
+		alt_Dugum->yukseklik = Yukseklik(alt_Dugum);
+		return alt_Dugum;
+	}
+	Dugum<Nesne>* SolCocukIleDegistir(Dugum<Nesne>* alt_Dugum)//sağa dönüş
+	{
+		Dugum<Nesne>* tmp = alt_Dugum->sol;
+		alt_Dugum->sol = tmp->sag;
+		tmp->sag = alt_Dugum;
+
+		// Yükseklikleri Güncelle 
+		alt_Dugum->yukseklik = Yukseklik(alt_Dugum);
+		tmp->yukseklik = max(Yukseklik(tmp->sol), alt_Dugum->yukseklik) + 1;
+
+		return tmp;
+	}
+	Dugum<Nesne>* SagCocukIleDegistir(Dugum<Nesne>* alt_Dugum)//sola dönüş
+	{
+		Dugum<Nesne>* tmp = alt_Dugum->sag;
+		alt_Dugum->sag = tmp->sol;
+		tmp->sol = alt_Dugum;
+
+		// Yükseklikleri Güncelle 
+		alt_Dugum->yukseklik = Yukseklik(alt_Dugum);
+		tmp->yukseklik = max(Yukseklik(tmp->sag), alt_Dugum->yukseklik) + 1;
+
+		return tmp;
+	}
+	void Inorder(Dugum<Nesne>* alt_Dugum) const {
+		if (alt_Dugum != NULL) {
+			Inorder(alt_Dugum->sol);
+			cout << alt_Dugum->veri << " ";
+			Inorder(alt_Dugum->sag);
+		}
+	}
+	void Preorder(Dugum<Nesne>* alt_Dugum) const {
+		if (alt_Dugum != NULL) {
+			cout << alt_Dugum->veri << " ";
+			Preorder(alt_Dugum->sol);
+			Preorder(alt_Dugum->sag);
+		}
+	}
+	void Postorder(Dugum<Nesne>* alt_Dugum) const {
+		if (alt_Dugum != NULL) {
+			Postorder(alt_Dugum->sol);
+			Postorder(alt_Dugum->sag);
+			cout << alt_Dugum->veri << " ";
+		}
+	}
+	void SeviyeyiYazdir(Dugum<Nesne>* alt_Dugum, int seviye)const {
+		if (alt_Dugum == NULL) return;
+		if (seviye == 0) cout << alt_Dugum->veri << " ";
+		else {
+			SeviyeyiYazdir(alt_Dugum->sol, seviye - 1);
+			SeviyeyiYazdir(alt_Dugum->sag, seviye - 1);
+		}
+	}
+	int Yukseklik(Dugum<Nesne>* alt_Dugum)const {
+		if (alt_Dugum == NULL)
+			return -1; // Ortada düğüm yoksa yükseklik anlamsızdır. Kodun çalışması adına -1 verilmektedir.
+		return 1 + max(Yukseklik(alt_Dugum->sol), Yukseklik(alt_Dugum->sag));
+	}
+	bool DugumSil(Dugum<Nesne>*& alt_Dugum)
+	{
+		Dugum<Nesne>* silinecekDugum = alt_Dugum;
+
+		if (alt_Dugum->sag == NULL)//sağ çocuğu yoksa
+			alt_Dugum = alt_Dugum->sol;//sol çocuğu yerine geçsin
+		else if (alt_Dugum->sol == NULL)//sol çocuğu yoksa
+			alt_Dugum = alt_Dugum->sag;//sağ çocuğu yerine geçsin
+		else {//iki çocuğu da varsa solun en sağı yerine geçsin
+			silinecekDugum = alt_Dugum->sol;
+			Dugum<Nesne>* ebeveynDugum = alt_Dugum;
+			while (silinecekDugum->sag != NULL) {
+				ebeveynDugum = silinecekDugum;
+				silinecekDugum = silinecekDugum->sag;
+			}
+			alt_Dugum->veri = silinecekDugum->veri;
+			if (ebeveynDugum == alt_Dugum)
+				alt_Dugum->sol = silinecekDugum->sol;
+			else
+				ebeveynDugum->sag = silinecekDugum->sol;
+		}
+		delete silinecekDugum;
+		return true;
+	}
+	bool Ara(Dugum<Nesne>* alt_Dugum, const Nesne& aranan) {
+		if (alt_Dugum == NULL)
+			return false;
+		if (alt_Dugum->veri == aranan)
+			return true;
+		if (aranan < alt_Dugum->veri)
+			return Ara(alt_Dugum->sol, aranan);
+		if (aranan > alt_Dugum->veri)
+			return Ara(alt_Dugum->sag, aranan);
+	}
+public:
+	AVLTree() {
+		root = NULL;
+	}
+	bool Bosmu() const {
+		return root == NULL;
+	}
+	void Ekle(const Nesne& yeni)
+	{
+		root = AraveEkle(root, yeni);
+	}
+	void Inorder() const {
+		Inorder(root);
+	}
+	void Preorder() const {
+		Preorder(root);
+	}
+	void Postorder() const {
+		Postorder(root);
+	}
+	void Levelorder()const {
+		int h = Yukseklik();
+		for (int level = 0; level <= h; level++)
+			SeviyeyiYazdir(root, level);
+	}
+	bool Arama(const Nesne& aranan) {
+		return Ara(root, aranan);
+	}
+	void Temizle() {
+		while (!Bosmu()) DugumSil(root);
+	}
+	int Yukseklik()const {
+		return Yukseklik(root);
+	}
+	~AVLTree() {
+		Temizle();
+	}
+};
+
+#endif
+//Test.cpp
+#include "AVLTree.hpp"
+
+int main() {
+	AVLTree<int>* agac = new AVLTree<int>();
+	agac->Ekle(15);
+	agac->Ekle(1);
+	agac->Ekle(157);
+	agac->Ekle(215);
+	agac->Ekle(120);
+	agac->Ekle(132);
+	agac->Ekle(310);
+	agac->Ekle(500);
+	agac->Ekle(125);
+	agac->Ekle(137);
+	agac->Ekle(300);
+	agac->Ekle(115);
+	cout << "Inorder :";
+	agac->Inorder();
+	cout << endl << "Preorder :";
+	agac->Preorder();
+	cout << endl << "Postorder :";
+	agac->Postorder();
+	cout << endl << endl;
+	cout << endl << "Level Order :";
+	agac->Levelorder();
+	cout << endl << endl;
+	cout << "Yukseklik:" << agac->Yukseklik() << endl;
+	delete agac;
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / bin / test . / src / test.cpp
+//
+//calistir :
+//. / bin / test
+
---
+++ HeapTree/HeapTree.h
@@ -0,0 +1,145 @@
+﻿#pragma once
+//HeapTree.hpp
+#ifndef HEAPTREE_HPP
+#define HEAPTREE_HPP
+
+#include <iostream>
+using namespace std;
+
+//Min-Heap Tree
+class HeapTree {
+private:
+	int* elemanlar;
+	int dugumSayisi;
+	int kapasite;
+
+	int SolCocukIndeks(int);
+	int SagCocukIndeks(int);
+	int EbeveynDugumIndeks(int);
+	void HeapYukari(int);
+	void HeapAsagi(int);
+public:
+	HeapTree(int);
+	bool Bosmu();
+	void Ekle(int);
+	int EnKucuguCikar();
+	~HeapTree();
+};
+#endif
+//HeapTree.cpp
+#include "HeapTree.hpp"
+
+int HeapTree::SolCocukIndeks(int dugumIndeks) {
+	return 2 * dugumIndeks + 1;
+}
+int HeapTree::SagCocukIndeks(int dugumIndeks) {
+	return 2 * dugumIndeks + 2;
+}
+int HeapTree::EbeveynDugumIndeks(int dugumIndeks) {
+	return (dugumIndeks - 1) / 2;
+}
+HeapTree::HeapTree(int kapasite) {
+	elemanlar = new int[kapasite];
+	dugumSayisi = 0;
+	this->kapasite = kapasite;
+}
+bool HeapTree::Bosmu() {
+	return (dugumSayisi == 0);
+}
+
+void HeapTree::HeapYukari(int dugumIndeks) {
+	int ebeveynIndeks, tmpVeri;
+	if (dugumIndeks != 0) {//Köke gelene kadar kaydır
+		ebeveynIndeks = EbeveynDugumIndeks(dugumIndeks);
+		if (elemanlar[ebeveynIndeks] > elemanlar[dugumIndeks]) {//ebevyn daha büyükse çocukla yerini değiştir
+			tmpVeri = elemanlar[ebeveynIndeks];
+			elemanlar[ebeveynIndeks] = elemanlar[dugumIndeks];
+			elemanlar[dugumIndeks] = tmpVeri;
+			HeapYukari(ebeveynIndeks);
+		}
+	}
+}
+
+void HeapTree::HeapAsagi(int dugumIndeks) {
+	int solCocukIndeks, sagCocukIndeks, enKucukIndeks, tmpVeri;
+	solCocukIndeks = SolCocukIndeks(dugumIndeks);
+	sagCocukIndeks = SagCocukIndeks(dugumIndeks);
+
+	if (sagCocukIndeks >= dugumSayisi) {//sağ çocucuğu yoksa
+		if (solCocukIndeks >= dugumSayisi) return;//ve sol çocuğu da yoksa çık
+		else
+			enKucukIndeks = solCocukIndeks;//sağ çocuğu yoksa ama sol çocuğu yoksa sol çocuk yeni küçük olur
+	}
+	else {//sağ çocuk varsa
+		if (elemanlar[solCocukIndeks] <= elemanlar[sagCocukIndeks])//ve sol çocuk sağ çocuktan küçük veya eşitse
+			enKucukIndeks = solCocukIndeks;//sol çocuk yeni küçük çocuk olur
+		else
+			enKucukIndeks = sagCocukIndeks;//sağ çocuk sol çocuktan küçükse sağ çocuk yeni küçük olur
+	}
+	if (elemanlar[dugumIndeks] > elemanlar[enKucukIndeks]) {//hangi çocuğun yeni küçük olduğu belirlendikten sonra çocukla ebeveyn yer değişir
+		tmpVeri = elemanlar[enKucukIndeks];
+		elemanlar[enKucukIndeks] = elemanlar[dugumIndeks];
+		elemanlar[dugumIndeks] = tmpVeri;
+		HeapAsagi(enKucukIndeks);//işlem aşağı yönde devam ederek ağaç tekrar heapleştirir
+	}
+}
+
+void HeapTree::Ekle(int sayi) {
+	if (dugumSayisi == kapasite)
+		return;
+	elemanlar[dugumSayisi] = sayi;//dizinin son indisi aynı zamanda ağacın en alt sağı olacağından yeni eleman buraya eklenir
+	dugumSayisi++;
+	HeapYukari(dugumSayisi - 1);//Heapleştirme işlemi
+}
+
+int HeapTree::EnKucuguCikar() {
+	if (Bosmu())
+		return -1;
+	int enKucuk = elemanlar[0];//Kök ağaçtaki en küçük eleman olacağı için kökü alır
+	elemanlar[0] = elemanlar[dugumSayisi - 1];//En alt sağdaki elemanı kök yapar
+	dugumSayisi--;
+	if (dugumSayisi > 0)
+		HeapAsagi(0);//Kökten başlayarak ağacı tekrar heapleştirir
+
+	return enKucuk;
+}
+
+HeapTree::~HeapTree() {
+	delete[] elemanlar;
+}
+//Test.cpp
+#include "HeapTree.hpp"
+#include <cstdlib>
+
+int main() {
+	HeapTree* heapAgaci = new HeapTree(100);
+	int* dizi = new int[25];
+
+	cout << "Dizi ilk Hali:" << endl;
+	for (int i = 0; i < 25; i++) {
+		dizi[i] = rand() % 100 + 1;
+		heapAgaci->Ekle(dizi[i]);
+		cout << dizi[i] << " ";
+	}
+	int indeks = 0;
+	while (!heapAgaci->Bosmu()) dizi[indeks++] = heapAgaci->EnKucuguCikar();
+
+	cout << endl << "Dizi Siralanmis Hali:" << endl;
+	for (int i = 0; i < 25; i++) {
+		cout << dizi[i] << " ";
+	}
+	cout << endl;
+	delete[] dizi;
+	delete heapAgaci;
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / lib / HeapTree.o - c . / src / HeapTree.cpp
+//g++ - I . / include / -o . / bin / test . / lib / HeapTree.o . / src / test.cpp
+//
+//calistir :
+//. / bin / test
+
