3faaa28c613247f90040140b29120d0512ed20c4
FurkanHome <FurkanHome@FURKANPC>
2020-08-26 09:11:56 +0300

    Huffman & hashing

---
+++ Huffman_Hashing/Hash.h
@@ -0,0 +1,292 @@
+﻿#pragma once
+//Open Hashing - Seperate Chaining
+//Hash.hpp
+#ifndef HASH_HPP
+#define HASH_HPP
+
+#include <iostream>
+using namespace std;
+#define MAX 101
+
+struct Dugum {
+	int veri;
+	Dugum* ileri;
+	Dugum(int veri, Dugum* ileri = NULL) {
+		this->veri = veri;
+		this->ileri = ileri;
+	}
+};
+//Separate Chaining - Ayrık Zincirleme
+class Hash {
+private:
+	Dugum* Dizi[MAX];
+public:
+	Hash();
+	int HashCode(int);
+	void Ekle(int);
+	bool Cikar(int);
+	void Temizle();
+	bool Bul(int);
+	bool Bosmu() const;
+	void Yaz() const;
+	~Hash();
+};
+#endif
+//Hash.cpp
+#include "Hash.hpp"
+
+Hash::Hash() {
+	for (int i = 0; i < MAX; i++)
+		Dizi[i] = NULL;
+}
+
+void Hash::Ekle(int sayi) {
+	if (Bul(sayi)) {
+		cout << sayi << " zaten var!" << endl;
+		return;
+	}
+	int indeks = HashCode(sayi);
+	Dizi[indeks] = new Dugum(sayi, Dizi[indeks]);
+}
+bool Hash::Bul(int sayi) {
+	int indeks = HashCode(sayi);
+	Dugum* tmp = Dizi[indeks];
+	while (tmp != NULL) {
+		if (tmp->veri == sayi)
+			return true;
+		tmp = tmp->ileri;
+	}
+	return false;
+}
+bool Hash::Cikar(int sayi) {
+	int indeks = HashCode(sayi);
+	if (Dizi[indeks] == NULL)
+		return false;
+	if (Dizi[indeks]->veri == sayi) {//ilk düğümse direkt sil
+		Dugum* sil = Dizi[indeks];
+		Dizi[indeks] = Dizi[indeks]->ileri;
+		delete sil;
+		return true;
+	}
+	Dugum* onceki = Dizi[indeks];//ilk düğüm değilse bulana kadar ilerle sonra sil
+	while (onceki->ileri != NULL) {
+		if (onceki->ileri->veri == sayi) {
+			Dugum* sil = onceki->ileri;
+			onceki->ileri = sil->ileri;
+			delete sil;
+			return true;
+		}
+		onceki = onceki->ileri;
+	}
+	return false;
+}
+
+bool Hash::Bosmu() const {
+	for (int i = 0; i < MAX; i++) {
+		if (Dizi[i] != NULL)
+			return false;
+	}
+	return true;
+}
+
+int Hash::HashCode(int sayi) {
+	return sayi % MAX;//h(x) = x%101 hash fonksiyonu
+}
+
+void Hash::Temizle() {
+	for (int i = 0; i < MAX; i++) {
+		Dugum* tmp = Dizi[i];
+		while (tmp != NULL) {
+			Dugum* sil = tmp;
+			tmp = tmp->ileri;
+			delete sil;
+		}
+		Dizi[i] = NULL;
+	}
+}
+Hash::~Hash() {
+	Temizle();
+}
+
+void Hash::Yaz() const {
+	for (int i = 0; i < MAX; i++) {
+		if (Dizi[i] != NULL) {
+			cout << "Dizi[" << i << "]=";
+			for (Dugum* tmp = Dizi[i]; tmp != NULL; tmp = tmp->ileri) {
+				cout << tmp->veri << "->";
+			}
+			cout << "NULL" << endl;
+		}
+	}
+}
+//test.cpp
+#include "Hash.hpp"
+
+int main() {
+	Hash* hash = new Hash();
+	hash->Ekle(100);
+	hash->Ekle(55);
+	hash->Ekle(1235);
+	hash->Ekle(10000);
+	hash->Ekle(1);
+	hash->Ekle(102);
+	hash->Ekle(102);
+	hash->Ekle(102);
+	hash->Yaz();
+	delete hash;
+
+	return 0;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / lib / Hash.o - c . / src / Hash.cpp
+//g++ - I . / include / -o . / bin / test . / lib / Hash.o . / src / test.cpp
+//
+//calistir :
+//. / bin / test
+
+//Double Hashing
+//DoubleHashing.hpp
+#ifndef DOUBLEHASHING_HPP
+#define DOUBLEHASHING_HPP
+
+#include <iostream>
+using namespace std;
+
+#define MAX 7
+#define YAKINASAL 5
+
+
+// Eksi değer kabul edilmeyen Double Hashing
+class DoubleHashing {
+private:
+	int* tablo;
+	int elemanSayisi;
+
+public:
+	DoubleHashing();
+	int Hash1(int);
+	int Hash2(int);
+	bool Dolumu()const;
+	void Ekle(int);
+	bool Ara(int);
+	void Cikar(int);
+	void Temizle();
+	void Yaz();
+	~DoubleHashing();
+};
+#endif
+//DoubleHashing.cpp
+#include "DoubleHashing.hpp"
+
+DoubleHashing::DoubleHashing() {
+	tablo = new int[MAX];
+	Temizle();
+}
+int DoubleHashing::Hash1(int anahtar) {
+	return anahtar % MAX;
+}
+int DoubleHashing::Hash2(int anahtar) {
+	return YAKINASAL - (anahtar % YAKINASAL);
+}
+bool DoubleHashing::Dolumu()const {
+	return elemanSayisi == MAX;
+}
+void DoubleHashing::Ekle(int anahtar) {
+	if (Dolumu())
+		return;
+	int indeks = Hash1(anahtar);
+	if (tablo[indeks] != -1) { // Çarpışma Olur
+		int adimSayisi = Hash2(anahtar);
+		int i = 1;
+		while (true) {
+			int yeniIndeks = (indeks + i * adimSayisi) % MAX;//sona gelince başa sarması için
+			if (tablo[yeniIndeks] == -1) { // Çarpışma yoksa ekle. varsa zaten while içinde yeni indis oluşturup tekrar deneyecek
+				tablo[yeniIndeks] = anahtar;
+				break;//ekleyince çık
+			}
+			i++;
+		}
+	}
+	else {
+		tablo[indeks] = anahtar;//boşsa direkt ekle
+	}
+	elemanSayisi++;
+}
+bool DoubleHashing::Ara(int anahtar) {
+	if (elemanSayisi == 0)
+		return false;
+	int indeks = Hash1(anahtar);
+	if (tablo[indeks] == anahtar)
+		return true;
+	int adimSayisi = Hash2(anahtar);
+	int i = 1;
+	while (i <= elemanSayisi) {
+		int yeniIndeks = (indeks + i * adimSayisi) % MAX;
+		if (tablo[yeniIndeks] == anahtar)
+			return true;
+		i++;
+	}
+	return false;
+}
+void DoubleHashing::Cikar(int anahtar) {
+	if (elemanSayisi == 0)
+		return;
+	int indeks = Hash1(anahtar);
+	if (tablo[indeks] == anahtar) {//ilk hashle hesaplanan indisteyse -1 yapar yani silip çıkar
+		tablo[indeks] = -1;
+		elemanSayisi--;
+		return;
+	}
+	int adimSayisi = Hash2(anahtar);
+	int i = 1;
+	while (i <= elemanSayisi) {//eğer ilk hash ile hesaplanamazsa ikinci hash ile hesaplanıp eleman bulunmaya çalışılır
+		int yeniIndeks = (indeks + i * adimSayisi) % MAX;
+		if (tablo[yeniIndeks] == anahtar) {//eleman bulununca silinir
+			tablo[yeniIndeks] = -1;
+			elemanSayisi--;
+			return;
+		}
+		i++;
+	}
+	cout << "eleman yok" << endl;
+}
+void DoubleHashing::Temizle() {
+	elemanSayisi = 0;
+	for (int i = 0; i < MAX; i++)
+		tablo[i] = -1;
+}
+void DoubleHashing::Yaz() {
+	for (int i = 0; i < MAX; i++)
+	{
+		if (tablo[i] != -1)
+			cout << "[" << i << "]\t:" << tablo[i] << endl;
+		else
+			cout << "[" << i << "]\t:" << endl;
+	}
+}
+DoubleHashing::~DoubleHashing() {
+	delete[] tablo;
+}
+//test.cpp
+#include "DoubleHashing.hpp"
+
+int main() {
+	int dizi[] = { 76,93,40,47,10,55 };
+	DoubleHashing* tablo = new DoubleHashing();
+	for (int i = 0; i < 6; i++)
+		tablo->Ekle(dizi[i]);
+	tablo->Yaz();
+	delete tablo;
+}
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / lib / DoubleHashing.o - c . / src / DoubleHashing.cpp
+//g++ - I . / include / -o . / bin / Test . / lib / DoubleHashing.o . / src / Test.cpp
+//
+//calistir :
+//. / bin / Test
---
+++ Huffman_Hashing/Huffman.h
@@ -0,0 +1,229 @@
+﻿#pragma once
+//Dugum.hpp
+#ifndef DUGUM_HPP
+#define DUGUM_HPP
+
+#include <iostream>
+using namespace std;
+
+class Dugum {
+public:
+	char karakter;
+	int frekans;	// Dosyada kaç kez tekrarlandığı
+	string kod; 	// 01 kod karşılığı
+	Dugum* sol;
+	Dugum* sag;
+	Dugum(char kr, int frek, Dugum* sl = NULL, Dugum* sg = NULL);
+	bool Yaprakmi() const;
+	bool operator==(Dugum&);
+	bool operator!=(Dugum&);
+	bool operator>(Dugum&);
+};
+#endif
+//Huffman.hpp
+#ifndef HUFFMAN_HPP
+#define HUFFMAN_HPP
+
+#include <fstream>
+#include <cstring>
+
+#include "Dugum.hpp"
+
+class Huffman {
+private:
+	int harfler;  			// harflerin ASCII karşılıkları
+	string girdi;			// dosyadan okunan girdi
+	Dugum* root;
+
+	Dugum* AgacOlustur(int[]);
+	void KodOlustur(Dugum*, string);
+	int MinDugumIndeks(Dugum* dugumler[]);
+	void DosyaOku(string);
+	void inorder(Dugum*);
+	void SilDugum(Dugum*);
+public:
+	Huffman(string);
+	void Kodla();
+	friend ostream& operator<<(ostream&, Huffman&);
+	~Huffman();
+};
+#endif
+//Dugum.cpp
+#include "Dugum.hpp"
+
+Dugum::Dugum(char kr, int frek, Dugum* sl, Dugum* sg) {
+	karakter = kr;
+	frekans = frek;
+	sol = sl;
+	sag = sg;
+	kod = "";
+}
+bool Dugum::Yaprakmi() const {
+	if (sol == NULL && sag == NULL) return true;
+	return false;
+}
+bool Dugum::operator==(Dugum& sag) {
+	if (this->karakter == sag.karakter) return true;
+	else return false;
+}
+bool Dugum::operator!=(Dugum& sag) {
+	if (this->karakter != sag.karakter) return true;
+	else return false;
+}
+bool Dugum::operator>(Dugum& sag) {
+	if (this->frekans > sag.frekans) return true;
+	else return false;
+}
+//Huffman.cpp
+#include "Huffman.hpp"
+
+Huffman::Huffman(string dosyaYolu) {
+	harfler = 256;
+	girdi = "";
+	DosyaOku(dosyaYolu);
+	root = NULL;
+}
+void Huffman::DosyaOku(string dosyaYolu) {
+	string satir;
+	ifstream dosya(dosyaYolu.c_str());
+	if (dosya.is_open())
+	{
+		while (getline(dosya, satir))
+		{
+			girdi += satir;
+			girdi += '\n';
+		}
+		dosya.close();
+	}
+}
+void Huffman::Kodla() {
+	char karakterler[girdi.size() - 1];
+
+	strcpy(karakterler, girdi.c_str());
+
+	//Frekansları say
+	int frek[harfler];
+	// frekansları sıfırla
+	for (int i = 0; i < harfler; i++)
+		frek[i] = 0;
+
+	for (int i = 0; i < girdi.size() - 1; i++)
+		frek[karakterler[i]]++;
+
+	// Huffman ağacını oluştur
+	root = AgacOlustur(frek);
+
+	// 01 kodlar oluşturuluyor
+	KodOlustur(root, "");
+}
+Dugum* Huffman::AgacOlustur(int frek[]) {
+	// dosyada var olan karakterler için düğüm olustur.
+	Dugum* dugumler[harfler];
+	for (int i = 0; i < harfler; i++) {
+		if (frek[i] > 0) {
+			dugumler[i] = new Dugum((char)i, frek[i]);
+		}
+		else {
+			dugumler[i] = NULL;
+		}
+	}
+	while (true) {
+		int ilkIndeks = MinDugumIndeks(dugumler);// birinci en küçük düğüm			
+		Dugum* min1 = dugumler[ilkIndeks];
+		dugumler[ilkIndeks] = NULL;
+		int ikinciIndeks = MinDugumIndeks(dugumler);// ikinci en küçük düğüm
+		if (ikinciIndeks == -1) {
+			return min1;//ikinci en küçük bulunamadıysa (yani tek bir düğüm var ise) tek düğümlük ağaç oluşur min1 de kök olarak belirlenir ve çıkılır
+		}
+		Dugum* min2 = dugumler[ikinciIndeks];
+		dugumler[ikinciIndeks] = NULL;
+		dugumler[ilkIndeks] = new Dugum(0, min1->frekans + min2->frekans, min1, min2);//en küçük iki düğümden bir ağaç oluşturup kök değerinin iki düğümün frekansının toplamı olarak belirler
+	}
+
+}
+int Huffman::MinDugumIndeks(Dugum* dugumler[]) {
+	Dugum* min;
+	int minIndeks = -1;
+
+	// ilk null olmayan düğümü min al
+	for (int i = 0; i < harfler; i++) {//ascii tablosu(harfler)'nun tamamı dolu olmayacağı için ilk kullanılan yani dolu olan harf minimum kabul edilir
+		if (dugumler[i] != NULL) {
+			min = dugumler[i];
+			minIndeks = i;
+			break;
+		}
+	}
+
+	for (int i = 0; i < harfler; i++)//üstteki döngüde bulunan minimum değeri kendinden sonra gelen her dolu ascii harfiyle karşılaştırılır karşılaştırılan düğümün frekansı kendisininkinden küçükse o düğüm yeni min olur harfler (ascii) bitene kadar devam eder
+	{
+		if (dugumler[i] == NULL) continue;
+
+		if (*min > * (dugumler[i])) {
+			min = dugumler[i];
+			minIndeks = i;
+		}
+	}
+	return minIndeks;
+}
+void Huffman::KodOlustur(Dugum* alt_dugum, string kod) {
+	if (!alt_dugum->Yaprakmi()) {
+		KodOlustur(alt_dugum->sol, kod + '0');
+		KodOlustur(alt_dugum->sag, kod + '1');
+	}
+	else {
+		alt_dugum->kod = kod;
+	}
+}
+ostream& operator<<(ostream& ekran, Huffman& sag) {
+	ekran << "Huffman Agacindaki Karakterler ve Kodlari:" << endl;
+	sag.inorder(sag.root);
+	return ekran;
+}
+void Huffman::inorder(Dugum* alt_dugum) {
+	if (alt_dugum != NULL) {
+		inorder(alt_dugum->sol);
+		if (alt_dugum->Yaprakmi())
+			cout << alt_dugum->karakter << ":" << alt_dugum->kod << endl;
+		inorder(alt_dugum->sag);
+	}
+}
+void Huffman::SilDugum(Dugum* alt_dugum) {
+	if (alt_dugum == NULL)
+		return;
+	SilDugum(alt_dugum->sol);
+	SilDugum(alt_dugum->sag);
+	delete alt_dugum;
+	alt_dugum = NULL;
+}
+Huffman::~Huffman() {
+	// çöp oluşmaması için ağaç temizleniyor.
+	SilDugum(root);
+}
+//test.cpp
+#include "Huffman.hpp"
+
+int main() {
+	Huffman* huffman = new Huffman("dene.txt");
+	huffman->Kodla();
+	cout << *huffman;
+	delete huffman;
+	return 0;
+}
+//dene.txt
+//muallim
+//umut
+//alim
+//ulu
+//uma
+
+//makefile
+//hepsi: derle calistir
+//
+//derle :
+//g++ - I . / include / -o . / lib / Dugum.o - c . / src / Dugum.cpp
+//g++ - I . / include / -o . / lib / Huffman.o - c . / src / Huffman.cpp
+//g++ - I . / include / -o . / bin / test . / lib / Huffman.o . / lib / Dugum.o . / src / test.cpp
+//
+//calistir :
+//. / bin / test
+
